edc3eef6a06eb0d02d461e95453a4dd2b2d518a3
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2021-12-31 13:34:13 +0200

    Seed Project Files

---
+++ app.js
@@ -0,0 +1,118 @@
+
+//Displays the original image after being uploaded
+function displayOriginalImage(event) {
+  if (event.files.length != 0) {
+    if (checkFileName(event.files[0].name)) {
+      document.getElementById("inputImage").src = window.URL.createObjectURL(event.files[0]);
+      document.getElementById("originalImage").style.display = "initial";
+      document.getElementById("transformation").style.display = "initial";
+      document.getElementById("result").style.display = "none";
+    }
+  }
+}
+
+//Makes sure the uploaded file is a png or jpg image 
+function checkFileName(fileName) {
+  if (fileName == "") {
+    alert("Browse to upload a valid File with png or jpg extension");
+    return false;
+  }
+  else if (fileName.split(".")[1].toUpperCase() == "PNG" || fileName.split(".")[1].toUpperCase() == "JPG")
+    return true;
+  else {
+    alert("File with " + fileName.split(".")[1] + " is invalid. Upload a valid file with png or jpg extensions");
+    return false;
+  }
+}
+
+//Displays the corresponding form to the selected transformation and hides the other forms
+function showTransformForm() {
+  const increaseBrightnessForm = document.getElementById("increaseBrightnessForm");
+  const decreaseBrightnessForm = document.getElementById("decreaseBrightnessForm");
+  //Write your code here for the other forms
+
+  const mylist = document.getElementById("myList");
+
+  //Storing the type chosen in a variable
+  transformType = mylist.options[mylist.selectedIndex].text;
+
+  //Displaying to the user the type he chose by changing the text element of id= transformType to the selected type
+  document.getElementById("transformType").value = mylist.options[mylist.selectedIndex].text;
+
+  if (transformType == "Increase Brightness") {
+    document.getElementById("increaseBrightnessInputs").style.display = "initial";
+    document.getElementById("decreaseBrightnessInputs").style.display = "none";
+    document.getElementById("increaseContrastInputs").style.display = "none";
+    document.getElementById("decreaseContrastInputs").style.display = "none";
+
+  } else if (transformType == "Decrease Brightness") {
+    //write your code here
+
+  } else if (transformType == "Increase Contrast") {
+    //Write your code here
+
+  } else {
+    //Write your code here
+  }
+
+  // Listener to the event of submiting the increase brightness form
+  increaseBrightnessForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    var ib = document.getElementById("ib").value
+    increaseBrightness(Number(ib))
+  });
+  //Write your code here for EventListeners for the other forms using the constants you will create in the transform function
+
+
+  //Applies pixel-wise transformations to increase brightness
+  function increaseBrightness(ib) {
+    const img = document.getElementById("inputImage");
+    const canvas = document.getElementById("resultImage");
+    const ctx = canvas.getContext('2d');
+
+    var transformedImage = [];
+    var val;
+
+    //Images are displayed in the RGBA format so a greyscale pixel could look like (25,25,25,255)
+    rgba = getRGBAValues(img, canvas, ctx);
+
+    for (i = 0; i < img.width * img.height * 4; i += 4) {
+      val = rgba[i] + ib;
+      if (val > 255) {
+        val = 255;
+      }
+      transformedImage.push(val, val, val, rgba[i + 3]);
+    }
+
+    displayResultImage(img, transformedImage, ctx);
+
+  }
+
+  //Write your code here for three more functions for the other transformations
+
+
+
+  //Extracts rgba 1D array of all the pixels in the original image
+  function getRGBAValues(img, canvas, ctx) {
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0);
+
+    var rgba = ctx.getImageData(
+      0, 0, img.width, img.height
+    ).data;
+    return rgba;
+  }
+
+  //Displays the transformed image
+  function displayResultImage(img, transformedImage, ctx) {
+    //Get a pointer to the current location in the image.
+    var palette = ctx.getImageData(0, 0, img.width, img.height); //x,y,w,h
+    //Wrap your array as a Uint8ClampedArray
+    palette.data.set(new Uint8ClampedArray(transformedImage)); // assuming values 0..255, RGBA, pre-mult.
+    //Repost the data.
+    ctx.putImageData(palette, 0, 0);
+    document.getElementById("result").style.display = "initial";
+  }
+}  
